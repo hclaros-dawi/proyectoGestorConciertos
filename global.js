4474ae7355fa9c0ae083540e1b004f3e2c5fed60
hclaros-dawi <146749783+hclaros-dawi@users.noreply.github.com>
2024-11-01 22:02:33 +0100

    Update global.js

--- global.js
+++ global.js
@@ -10,30 +10,31 @@ const fecha = new Date();
 let dias, horas;
 
 function calculoHoras(fechaConcierto) {
-    return Math.ceil((fechaConcierto.getTime() - fecha.getTime())
-    /(1000 * 60 * 60));
+  return Math.ceil(
+    (fechaConcierto.getTime() - fecha.getTime()) / (1000 * 60 * 60)
+  );
 }
 
-function calculoDias(fechaConcierto) { 
-    const totalHoras = calculoHoras(fechaConcierto);
-    dias = parseInt(totalHoras / 24);
-    horas = totalHoras % 24;
-    return [dias, horas];
+function calculoDias(fechaConcierto) {
+  const totalHoras = calculoHoras(fechaConcierto);
+  dias = parseInt(totalHoras / 24);
+  horas = totalHoras % 24;
+  return [dias, horas];
 }
 
 //..................................................//
 //FUNCIÓN DETERMINAR TEMPORADA
 function determinarTemporada(fecha) {
-    const temporada = ["Invierno", "Primavera", "Verano", "Otoño"];
-    const mes = fecha.getMonth();
-    let index = parseInt(mes / 3);
-    if(fecha.getMonth() % 3 === 2 && fecha.getDate() >= 25){
-        index++;
-    }
-    if(index >= 4){
-        index = 0;
-    }
-    return temporada[index];
+  const temporada = ["Invierno", "Primavera", "Verano", "Otoño"];
+  const mes = fecha.getMonth();
+  let index = parseInt(mes / 3);
+  if (fecha.getMonth() % 3 === 2 && fecha.getDate() >= 25) {
+    index++;
+  }
+  if (index >= 4) {
+    index = 0;
+  }
+  return temporada[index];
 }
 
 //..................................................//
@@ -41,16 +42,32 @@ function determinarTemporada(fecha) {
 function programarRecordatorio(diasAntes) {
   const [diasRestantes, horasRestantes] = calculoDias(fechaConcierto);
 
-    if(diasRestantes < diasAntes){
-        console.log(diasRestantes < 0 ?  "No puedes poner una fecha anterior"
-            : "Queda" + (dias === 1  ? " " +  diasRestantes + " dia" :
-                diasRestantes > 1 ? "n " + diasRestantes + " días" :
-                    diasRestantes === 0 && horasRestantes > 1 ? "n" : "") +
+  if (diasRestantes < diasAntes) {
+    console.log(
+      diasRestantes < 0
+        ? "No puedes poner una fecha anterior"
+        : "Queda" +
+            (dias === 1
+              ? " " + diasRestantes + " dia"
+              : diasRestantes > 1
+              ? "n " + diasRestantes + " días"
+              : diasRestantes === 0 && horasRestantes > 1
+              ? "n"
+              : "") +
             (diasRestantes > 0 ? " y" : "") +
-            (horasRestantes === 1 ? " " + + horasRestantes + " hora" :
-                horasRestantes > 1 ? " " + horasRestantes + " horas" : ""))
-        console.log("El concierto es el " + fechaConcierto.toLocaleString + " en la estación de " + determinarTemporada(fechaConcierto))
-    }
+            (horasRestantes === 1
+              ? " " + +horasRestantes + " hora"
+              : horasRestantes > 1
+              ? " " + horasRestantes + " horas"
+              : "")
+    );
+    console.log(
+      "El concierto es el " +
+        fechaConcierto.toLocaleString +
+        " en la estación de " +
+        determinarTemporada(fechaConcierto)
+    );
+  }
 }
 //.toLocaleString()-->de obj date a string
 
@@ -58,7 +75,7 @@ function programarRecordatorio(diasAntes) {
 //FUNCIÓN IMPRIMIR EVENTOS
 function imprimirEventos() {
   const contenedorEventos = document.getElementById("eventos");
-  contenedorEventos.innerHTML = ""; 
+  contenedorEventos.innerHTML = "";
 
   eventos.forEach((evento) => {
     const fechaConcierto = new Date(evento.fecha);
@@ -72,7 +89,9 @@ function imprimirEventos() {
 <div class="contenedor-form">
            <div class="evento-fecha">
                <p class="fecha dia">${fechaConcierto.getDate()}</p>
-               <p class="fecha mes">${fechaConcierto.toLocaleString("en", { weekday: "long" })}
+               <p class="fecha mes">${fechaConcierto.toLocaleString("en", {
+                 weekday: "long",
+               })}
            </div>
            <div class="evento-info">
                <h1>${evento.nombre}</h1>
@@ -84,7 +103,11 @@ function imprimirEventos() {
            </div>
            <img src="03_Print_A3_Halloween_Deathlight.jpg" alt="Imagen del evento" class="evento-imagen">
            <br><br>
-           <button class="evento-boton" onclick="mostrarDescripcion('${evento.nombre}', '${evento.descripcion}', '${fechaConcierto.toISOString()}')">Más Info</button>
+           <button class="evento-boton" onclick="mostrarDescripcion('${
+             evento.nombre
+           }', '${
+      evento.descripcion
+    }', '${fechaConcierto.toISOString()}')">Más Info</button>
            <p id="descripcionEvento" style="margin-top: 20px; font-weight: bold;"></p>  
     <div class="evento-compra">
         <h2>Comprar entradas</h2>
@@ -124,24 +147,24 @@ function imprimirEventos() {
 //..................................................//
 //FUNCIÓN PARA GENERARA ID CONCIERTO (ALEATORIO)
 function generarIDConcierto(fechaConcierto, precioBase) {
-    const milisegundosFecha = fechaConcierto.getTime(); //Fecha a milisegundos
-    const precioMultiplicado = Math.round(precioBase * 2000); //Multiplicar el precio base por 2000 por ejemplo
-    const idUnico = milisegundosFecha + precioMultiplicado;
-    
-    return idUnico;
+  const milisegundosFecha = fechaConcierto.getTime(); //Fecha a milisegundos
+  const precioMultiplicado = Math.round(precioBase * 2000); //Multiplicar el precio base por 2000 por ejemplo
+  const idUnico = milisegundosFecha + precioMultiplicado;
+
+  return idUnico;
 }
 
 //..................................................//
 //FUNCIÓN CREAR EVENTO
 function crearEvento() {
-  //Creo objecto fecha con hora 
+  //Creo objecto fecha con hora
   const hora = document.querySelector('input[type="time"]').value;
-  const fecha = document.querySelector('#fechaConcierto').value;
+  const fecha = document.querySelector("#fechaConcierto").value;
   fechaConcierto = new Date(`${fecha}T${hora}:00`);
   let nombre = document.querySelector(
     'input[placeholder="Nombre del evento"]'
   ).value;
-  nombre = formatearNombreConcierto(nombre);  
+  nombre = formatearNombreConcierto(nombre);
   const capacidad = document.querySelector(
     'input[placeholder="Capacidad"]'
   ).value;
@@ -150,23 +173,33 @@ function crearEvento() {
   ).value;
   artista = validarNombreArtista(artista);
   const idConcierto = generarIDConcierto(fechaConcierto, precioBase);
-  const nuevoEvento = {id: idConcierto, nombre, fecha: fechaConcierto, capacidad, descripcion: artista, hora};
+  const nuevoEvento = {
+    id: idConcierto,
+    nombre,
+    fecha: fechaConcierto,
+    capacidad,
+    descripcion: artista,
+    hora,
+  };
 
   eventos.push(nuevoEvento);
   imprimirEventos();
 }
 
 //EVENT LISTENERS
-document.querySelector(".CreacionInformacion button").addEventListener("click", crearEvento);
+document
+  .querySelector(".CreacionInformacion button")
+  .addEventListener("click", crearEvento);
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("crear-evento").addEventListener("click", crearEvento);
-    const botonSticky = document.querySelector(".boton-sticky");
-    const formulario = document.querySelector(".CreacionInformacion");
-    botonSticky.addEventListener("click", () => {
-        formulario.classList.toggle("visible");
-    });
+  document
+    .getElementById("crear-evento")
+    .addEventListener("click", crearEvento);
+  const botonSticky = document.querySelector(".boton-sticky");
+  const formulario = document.querySelector(".CreacionInformacion");
+  botonSticky.addEventListener("click", () => {
+    formulario.classList.toggle("visible");
+  });
 });
-
 
 //..................................................//
 //FUNCIÓN CALCULAR PRECIO ENTRADA
@@ -176,7 +209,8 @@ function calcularPrecioEntrada() {
   const numPerson = parseInt(document.querySelector("#personas").value);
 
   if (isNaN(numPerson) || numPerson <= 0) {
-    document.querySelector("#resultado").innerHTML = "Introduce un número válido de personas";
+    document.querySelector("#resultado").innerHTML =
+      "Introduce un número válido de personas";
     return;
   }
 
@@ -202,7 +236,9 @@ function calcularPrecioEntrada() {
       break;
   }
 
-  document.querySelector("#resultado").innerHTML = `Precio Total: ${precioFinal.toFixed(2)}€`;
+  document.querySelector(
+    "#resultado"
+  ).innerHTML = `Precio Total: ${precioFinal.toFixed(2)}€`;
   numEntradas += numPerson; //Actualiza el total de entradas compradas
 }
 
@@ -214,8 +250,9 @@ function calcularIngresosEsperados(numPerson) {
   const precioBase = 30.25;
 
   if (isNaN(numPerson) || numPerson <= 0) {
-    document.getElementById("mensajeIngreso").innerHTML = "Error: El número de personas es inválido";  
-    return;  
+    document.getElementById("mensajeIngreso").innerHTML =
+      "Error: El número de personas es inválido";
+    return;
   }
 
   const ingresoTotal = numPerson * precioBase;
@@ -224,7 +261,11 @@ function calcularIngresosEsperados(numPerson) {
   const cantidadSala = ingresoTotal - cantidadArtista;
   const ingresoPorAsistente = ingresoTotal / numPerson;
 
-  document.getElementById("mensajeIngreso").innerHTML = `Los ingresos esperados totales son: ${ingresoTotal.toFixed(2)}€ <br>
+  document.getElementById(
+    "mensajeIngreso"
+  ).innerHTML = `Los ingresos esperados totales son: ${ingresoTotal.toFixed(
+    2
+  )}€ <br>
          Los ingresos por asistente son: ${ingresoPorAsistente.toFixed(2)}€ <br>
          Cantidad destinada al artista: ${cantidadArtista.toFixed(2)}€ <br>
          Cantidad destinada a la sala: ${cantidadSala.toFixed(2)}€`;
@@ -234,39 +275,43 @@ function calcularIngresosEsperados(numPerson) {
 //FUNCIÓN FORMATEAR NOMBRE
 function formatearNombreConcierto(nombre) {
   return nombre
-      .toLowerCase() //Convierte todo el texto a minúsculas
-      .split(" ") //Divide el texto en palabras
-      .map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)) //Convierte la primera letra a mayúscula
-      .join(" ");  //Une las palabras con un espacio
+    .toLowerCase() //Convierte todo el texto a minúsculas
+    .split(" ") //Divide el texto en palabras
+    .map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1)) //Convierte la primera letra a mayúscula
+    .join(" "); //Une las palabras con un espacio
 }
 
 //..................................................//
 //FUNCIÓN PARA CREAR Y MOSTRAR DESCRIPCIÓN (+ INFO)
 function mostrarDescripcion(nombreConcierto, nombreArtista, fecha) {
+  const descripcion = crearDescripcionEvento(
+    nombreConcierto,
+    nombreArtista,
+    fecha
+  );
+  const contenedorDescripcion = document.getElementById("descripcionEvento");
 
-  const descripcion = crearDescripcionEvento(nombreConcierto, nombreArtista, fecha);
-  const contenedorDescripcion = document.getElementById("descripcionEvento");  
-  
   contenedorDescripcion.innerHTML = descripcion;
 }
 
 function crearDescripcionEvento(nombreConcierto, nombreArtista, fecha) {
-  const opcionesFecha = { day: 'numeric', month: 'long' };
-  const fechaFormateada = new Date(fecha).toLocaleDateString("es-ES", opcionesFecha);
-  
+  const opcionesFecha = { day: "numeric", month: "long" };
+  const fechaFormateada = new Date(fecha).toLocaleDateString(
+    "es-ES",
+    opcionesFecha
+  );
+
   return `Concierto de ${nombreConcierto} con ${nombreArtista} el ${fechaFormateada}.`;
 }
 
 //..................................................//
 //FUNCIÓN PARA VALIDAR NOMBRE ARTISTA
 function validarNombreArtista(nombre) {
-
   let nombreFormateado = nombre.trim(); //Elimina espacios en blanco al principio y al final
-  nombreFormateado = nombreFormateado.replace(/\s+/g, ' '); //Elimina espacios duplicados en el medio
+  nombreFormateado = nombreFormateado.replace(/\s+/g, " "); //Elimina espacios duplicados en el medio
 
   return nombreFormateado;
 }
-
 
 //..................................................//
 //FUNCIÓN PARA VALIDAR NÚMERO DE ENTRADAS
@@ -274,11 +319,11 @@ const maxEntradas = 200;
 function validarEntradasDisponibles() {
   if (numEntradas < 0) {
     return "Error: El número de entradas ingresadas no es válido.";
-}
-if (numEntradas > maxEntradas) {
+  }
+  if (numEntradas > maxEntradas) {
     return "Error: Ya no hay entradas disponibles.";
-}
-return true;  
+  }
+  return true;
 }
 
 //..................................................//
@@ -286,13 +331,14 @@ return true;
 function gestionComprarEntradas() {
   const resultadoValidacion = validarEntradasDisponibles();
 
-  const mensajeElemento = document.getElementById('mensajeValidacion');
+  const mensajeElemento = document.getElementById("mensajeValidacion");
   if (resultadoValidacion === true) {
-      mensajeElemento.innerHTML = "Compra exitosa! Has comprado " + numEntradas + " tickets.";
-      mensajeElemento.style.color = "green";
+    mensajeElemento.innerHTML =
+      "Compra exitosa! Has comprado " + numEntradas + " tickets.";
+    mensajeElemento.style.color = "green";
   } else {
-      mensajeElemento.innerHTML = resultadoValidacion;
-      mensajeElemento.style.color = "red";
+    mensajeElemento.innerHTML = resultadoValidacion;
+    mensajeElemento.style.color = "red";
   }
 }
 
